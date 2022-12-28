e67c85a57176bb6de3f7b1d6c751a853335f3a95
dstamil <99588972+dstamil@users.noreply.github.com>
2022-12-28 15:21:13 +0530

    Update texts.py

--- texts.py
+++ texts.py
@@ -1,7 +1,7 @@
-HELP_TEXT = """👊Cᴏɴᴛᴀᴄᴛ ʀᴇᴘᴏʀᴛ ʙᴜɢ :-@BotCreator99"""
+HELP_TEXT = """👊JUST SEND LINK THEN SEE MAGIC"""
 
 ABOUT_TEXT = """
 <b>⚜ Mʏ ɴᴀᴍᴇ :- ShareusBypasserBot</b>\n
-<b>🔸Subscribe :- <a href='https://youtube.com/@GreyMattersYT'>GreyMatter's YT</a></b>\n
-<b>🔹Dᴇᴠᴇʟᴏᴘᴇʀ :- <a href='https://t.me/BotCreator99'>BotCreator99</a></b>\n
-<b>🔸Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ :- <a href='https://telegram.me/GreyMatter_Bots'>[ 19-12-2022 ]</a></b>"""
+<b>🔸Subscribe :- <a href='https://t.me/MM_MOVIESSS'>DS</a></b>\n
+<b>🔹Dᴇᴠᴇʟᴏᴘᴇʀ :- <a href='https://t.me/MM_MOVIESSS'>DS</a></b>\n
+<b>🔸Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ :- <a href='https://t.me/MM_MOVIESSS'>[ 19-12-2022 ]</a></b>"""
